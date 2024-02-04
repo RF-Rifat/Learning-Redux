eff95366009065a7a8cbb5ff813649399725eedb
RF-Rifat <rfrifat6344@gmail.com>
2024-02-04 10:24:20 +0600

    provider updated

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,22 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems } = useSelector((state) => state.cart);
+   const dispatch = useDispatch();
+   useEffect(() => {
+     dispatch(calculateTotals());
+   }, [cartItems, dispatch]);
+  return (
+    <main>
+      {/* <Modal/> */}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
